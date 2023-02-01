c8c36a1d4e099f166cdb97fcdc58a3f487491567
ik250692 <110094738+ik250692@users.noreply.github.com>
2023-02-01 13:08:49 +0600

    Update database.py

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ conn = psycopg2.connect(
             port = '5432',
             dbname = 'postgres')
 cursor = conn.cursor()
-sql = 'insert into tv_7(parsedate,link,status,title,newsdate,description,video_link) values(%s,%s,%s,%s,%s,%s,%s)', (datetime.now(), 'Telegram', 'Telegram', event.chat.title, datetime.now(), event.message.message, 'Empty')
+sql = 'insert into tv_7(date_parse,link,status,title,date_news,description,link_img) values(%s,%s,%s,%s,%s,%s,%s)', (datetime.date.today(), chats_name, 'Telegram', chats_name, message.date, message.message, 'Telegram')
 cursor.execute(sql)
 conn.commit()
 cursor.close()
